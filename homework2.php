d579c4e5f06c16c89833eff389f1ae10956d234f
Connor Heath <ctheath09gmail.com>
2025-02-10 19:32:14 -0500

    feat: add html boiler plate

--- homework2.php
+++ homework2.php
@@ -1,1 +1,14 @@
-# This is your HTML/PHP file. Delete this comment, and then get started according to the instructions!
+<?php
+    echo "<!DOCTYPE html>
+    <html lang='eng-US'>
+        <head>
+            <meta charset='utf-8'>
+            <meta name='viewport' content='width=device-width,initial-scale=1'/>
+        </head>
+        <body>
+            <main>
+                
+            </main>
+        </body>
+    </html>"
+?>
